8c49b5f3a78b751517ee9adfee3a198fa8cac039
Florian Müller <email.mufl@googlemail.com>
2012-05-01 19:42:57 +0200

    More sprite mappings for monsters and structures

--- org.blockout.engine/src/main/java/org/blockout/engine/SpriteType.java
+++ org.blockout.engine/src/main/java/org/blockout/engine/SpriteType.java
@@ -11,5 +11,35 @@ public enum SpriteType {
 	Crate, 
 	Player,
 	Skeleton,
-	Zombie
+	Zombie,
+	
+	// Monters
+	giant_ant, killer_bee, soldier_ant, fire_ant, giant_beetle,		
+	queen_bee, acid_blob, quivering_blob, gelatinous_cube,
+	chickatrice, cockatrice, pyrolisk, jackal, fox,
+	coyote, werejackal, little_dog, dog, large_dog,		
+	dingo, wolf, werewolf, warg, winter_wolf_cub,
+	winter_wolf, hell_hound_pup, hell_hound, Cerberus, gas_spore,		
+	floating_eye, freezing, sphere, flaming_sphere, shocking_sphere,
+	beholder, kitten, housecat, jaguar, lynx,
+	panther,large_cat,
+	
+	
+	// Structures
+	wallway_vertical, wallway_horizontal, wallway_corner_up_tight,
+	wallway_corner_up_left, wallway_corner_down_right,
+	wallway_corner_down_left, wallway_crossing,
+	wallway_intersection_horizontal_up, wallway_intersection_horizontal_down,
+	wallway_intersection_vertical_right, wallway_intersection_vertical_left,
+	
+	open_door,closed_door,iron_bars,tree,
+	stoneground,stoneground_dark,stoneground_mid,
+	staircase_up,staircase_down,ladder_up,
+	ladder_down,altar,grave,opulent_throne,		
+	sink,fountain,water,ice,molten_lava,
+	lowered_drawbridge_vertical,
+	lowered_drawbridge_horizontal,		
+	raised_drawbridge_vertical,
+	raised_drawbridge_horizontal,
+	
 }
