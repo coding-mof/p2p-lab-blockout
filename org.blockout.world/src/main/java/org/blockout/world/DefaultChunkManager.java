608b3d35d3c1f86a2ad019407066bbc0221a8145
coding-mof <coding.mof@gmail.com>
2012-05-31 00:34:52 +0200

    Fixeg bug

--- org.blockout.world/src/main/java/org/blockout/world/DefaultChunkManager.java
+++ org.blockout.world/src/main/java/org/blockout/world/DefaultChunkManager.java
@@ -27,26 +27,25 @@ import org.blockout.world.state.IStateMachineListener;
 public class DefaultChunkManager extends MessageReceiver implements IChunkManager, IStateMachineListener {
 
 	private IStateMachine												stateMachine;
-	
-	private final WorldAdapter 											worldAdapter;
-	
-	@Inject private final IMessagePassing								messegePassing;
-	
-	private final Hashtable<TileCoordinate, ArrayList<INodeAddress>> 	receiver;
 
-	
-	
+	private final WorldAdapter											worldAdapter;
+
 	@Inject
-	public DefaultChunkManager(	WorldAdapter worldAdapter, IMessagePassing network) {
+	private final IMessagePassing										messagePassing;
+
+	private final Hashtable<TileCoordinate, ArrayList<INodeAddress>>	receiver;
+
+	@Inject
+	public DefaultChunkManager(final WorldAdapter worldAdapter, final IMessagePassing network) {
 		super();
 		receiver = new Hashtable<TileCoordinate, ArrayList<INodeAddress>>();
 		this.worldAdapter = worldAdapter;
-		this.worldAdapter.setManager(this);
-		this.messegePassing = network;
-		network.addReceiver(this, ChuckRequestMessage.class);
-		network.addReceiver(this, ChunkDeliveryMessage.class);
-		network.addReceiver(this, StateMessage.class);
-		network.addReceiver(this, StopUpdatesMessage.class);
+		this.worldAdapter.setManager( this );
+		messagePassing = network;
+		network.addReceiver( this, ChuckRequestMessage.class );
+		network.addReceiver( this, ChunkDeliveryMessage.class );
+		network.addReceiver( this, StateMessage.class );
+		network.addReceiver( this, StopUpdatesMessage.class );
 	}
 
 	@Override
@@ -58,13 +57,11 @@ public class DefaultChunkManager extends MessageReceiver implements IChunkManage
 	 * {@inheritDoc}
 	 */
 	@Override
-	public void eventCommitted(final IEvent<?> event) {
-		TileCoordinate coordinate = Chunk.containingCunk(event
-				.getResponsibleTile());
-		if (receiver.containsKey(coordinate)) {
-			for (INodeAddress address : receiver.get(coordinate)) {
-				messegePassing.send(new StateMessage(event, StateMessage.COMMIT_MESSAGE),
-						address);
+	public void eventCommitted( final IEvent<?> event ) {
+		TileCoordinate coordinate = Chunk.containingCunk( event.getResponsibleTile() );
+		if ( receiver.containsKey( coordinate ) ) {
+			for ( INodeAddress address : receiver.get( coordinate ) ) {
+				messagePassing.send( new StateMessage( event, StateMessage.COMMIT_MESSAGE ), address );
 			}
 		}
 	}
@@ -73,14 +70,28 @@ public class DefaultChunkManager extends MessageReceiver implements IChunkManage
 	 * {@inheritDoc}
 	 */
 	@Override
-	public void eventPushed(final IEvent<?> event) {
-		TileCoordinate coordinate = Chunk.containingCunk(event
-				.getResponsibleTile());
-		if (receiver.containsKey(coordinate)) {
-			stateMachine.commitEvent(event);
-			eventCommitted(event);
+	public void eventPushed( final IEvent<?> event ) {
+		TileCoordinate coordinate = Chunk.containingCunk( event.getResponsibleTile() );
+		if ( receiver.containsKey( coordinate ) ) {
+			messagePassing.send( new StateMessage( event, StateMessage.Push_MESSAGE ), new Hash( coordinate ) );
 		} else {
-			messegePassing.send(new StateMessage(event,StateMessage.Push_MESSAGE), new Hash(coordinate));
+			// BUG? doesn't this raise another commitEvent ?
+			stateMachine.commitEvent( event );
+			eventCommitted( event );
+		}
+		// TODO add local connections
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void eventRolledBack( final IEvent<?> event ) {
+		TileCoordinate coordinate = Chunk.containingCunk( event.getResponsibleTile() );
+		if ( receiver.containsKey( coordinate ) ) {
+			for ( INodeAddress address : receiver.get( coordinate ) ) {
+				messagePassing.send( new StateMessage( event, StateMessage.ROLLBAK_MESSAGE ), address );
+			}
 		}
 
 		// TODO add local connections
@@ -90,74 +101,56 @@ public class DefaultChunkManager extends MessageReceiver implements IChunkManage
 	 * {@inheritDoc}
 	 */
 	@Override
-	public void eventRolledBack(final IEvent<?> event) {
-		TileCoordinate coordinate = Chunk.containingCunk(event
-				.getResponsibleTile());
-		if (receiver.containsKey(coordinate)) {
-			for (INodeAddress address : receiver.get(coordinate)) {
-				messegePassing.send(
-						new StateMessage(event, StateMessage.ROLLBAK_MESSAGE),
-						address);
-			}
-		}
-		
-		//TODO add local connections
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
 	public void requestChunk( final TileCoordinate position ) {
-		messegePassing.send(new ChuckRequestMessage(position), new Hash(position));
+		messagePassing.send( new ChuckRequestMessage( position ), new Hash( position ) );
 	}
 
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
-	public void stopUpdating(TileCoordinate position) {
-		messegePassing.send(new StopUpdatesMessage(position), new Hash(position));
+	public void stopUpdating( final TileCoordinate position ) {
+		messagePassing.send( new StopUpdatesMessage( position ), new Hash( position ) );
 	}
-	
-	public void  receive(StateMessage msg, INodeAddress origin){
-		switch (msg.getType()) {
-		case StateMessage.ROLLBAK_MESSAGE:
-			stateMachine.rollbackEvent(msg.getEvent());
-			break;
-		case StateMessage.COMMIT_MESSAGE:
-			stateMachine.commitEvent(msg.getEvent());
-			break;
-		case StateMessage.Push_MESSAGE:
-			stateMachine.pushEvent(msg.getEvent());
-			break;
-		default:
-			break;
+
+	public void receive( final StateMessage msg, final INodeAddress origin ) {
+		switch ( msg.getType() ) {
+			case StateMessage.ROLLBAK_MESSAGE:
+				stateMachine.rollbackEvent( msg.getEvent() );
+				break;
+			case StateMessage.COMMIT_MESSAGE:
+				stateMachine.commitEvent( msg.getEvent() );
+				break;
+			case StateMessage.Push_MESSAGE:
+				stateMachine.pushEvent( msg.getEvent() );
+				break;
+			default:
+				break;
 		}
 	}
-	
-	public void  receive(ChuckRequestMessage msg, INodeAddress origin) {
-		Chunk c =worldAdapter.getChunk(msg.getCoordinate());
-		if(!receiver.containsKey(c.getPosition())){
-			receiver.put(c.getPosition(), new ArrayList<INodeAddress>());
+
+	public void receive( final ChuckRequestMessage msg, final INodeAddress origin ) {
+		Chunk c = worldAdapter.getChunk( msg.getCoordinate() );
+		if ( !receiver.containsKey( c.getPosition() ) ) {
+			receiver.put( c.getPosition(), new ArrayList<INodeAddress>() );
 		}
-		receiver.get(c.getPosition()).add(origin);
-		messegePassing.send(new ChunkDeliveryMessage(c), origin);
+		receiver.get( c.getPosition() ).add( origin );
+		messagePassing.send( new ChunkDeliveryMessage( c ), origin );
 	}
-	
-	public void  receive(ChunkDeliveryMessage msg, INodeAddress origin) {
-		Chunk c =msg.getChunk();
-		if(receiver.containsKey(c.getPosition())){
-			receiver.get(c.getPosition()).remove(origin);
+
+	public void receive( final ChunkDeliveryMessage msg, final INodeAddress origin ) {
+		Chunk c = msg.getChunk();
+		if ( receiver.containsKey( c.getPosition() ) ) {
+			receiver.get( c.getPosition() ).remove( origin );
 		}
-		worldAdapter.responseChunk(c);
+		worldAdapter.responseChunk( c );
 	}
-	
-	public void  receive(StopUpdatesMessage msg, INodeAddress origin) {
-		if(receiver.containsKey(msg.getCoordinate())){
-			receiver.get(msg.getCoordinate()).remove(origin);
+
+	public void receive( final StopUpdatesMessage msg, final INodeAddress origin ) {
+		if ( receiver.containsKey( msg.getCoordinate() ) ) {
+			receiver.get( msg.getCoordinate() ).remove( origin );
 		}
-		//TODO remove from local connections
+		// TODO remove from local connections
 	}
 
 }
