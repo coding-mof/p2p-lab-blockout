90d2f60cc166e4ae68a399160e6bb1eefacba707
coding-mof <coding.mof@gmail.com>
2012-07-18 22:25:59 +0200

    Fixed serialization issue

--- org.blockout.network/src/main/java/org/blockout/network/reworked/IAmYourPredeccessor.java
+++ org.blockout.network/src/main/java/org/blockout/network/reworked/IAmYourPredeccessor.java
@@ -1,11 +1,14 @@
 package org.blockout.network.reworked;
 
+import java.io.Serializable;
+
 import org.blockout.network.dht.IHash;
 
 import com.google.common.base.Preconditions;
 
-public class IAmYourPredeccessor {
-	private final IHash	nodeId;
+public class IAmYourPredeccessor implements Serializable {
+	private static final long	serialVersionUID	= -3266094817926621826L;
+	private final IHash			nodeId;
 
 	public IAmYourPredeccessor(final IHash nodeId) {
 
