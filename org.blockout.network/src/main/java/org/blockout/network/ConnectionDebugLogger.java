edb18515c5b7cd0d6b3629f6a6df64901b48ebc8
Florian Müller <email.mufl@googlemail.com>
2012-07-18 21:55:26 +0200

    log chrod events

--- org.blockout.network/src/main/java/org/blockout/network/ConnectionDebugLogger.java
+++ org.blockout.network/src/main/java/org/blockout/network/ConnectionDebugLogger.java
@@ -7,8 +7,12 @@ import java.net.SocketAddress;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.blockout.network.dht.IHash;
+import org.blockout.network.dht.WrappedRange;
+import org.blockout.network.reworked.ChordListener;
 import org.blockout.network.reworked.ConnectionListener;
 import org.blockout.network.reworked.ConnectionManager;
+import org.blockout.network.reworked.IChordOverlay;
 import org.blockout.network.reworked.IConnectionManager;
 
 import com.google.common.base.Preconditions;
@@ -20,12 +24,25 @@ import com.google.common.base.Preconditions;
  * @author Florian Müller
  * 
  */
-public class ConnectionDebugLogger implements ConnectionListener {
+public class ConnectionDebugLogger implements ConnectionListener, ChordListener {
     private FileWriter       logWriter;
     private SimpleDateFormat dateFormat;
 
-    public ConnectionDebugLogger( final ConnectionManager connectionMgr ) {
+    private IChordOverlay    chordOverlay;
+    private IHash            localChordId;
+    private IHash            currentPredecessor;
+    private IHash            currentSuccessor;
+
+    public ConnectionDebugLogger( final ConnectionManager connectionMgr,
+            final IChordOverlay chordOverlay ) {
         Preconditions.checkNotNull( connectionMgr );
+
+        this.chordOverlay = chordOverlay;
+        this.chordOverlay.addChordListener( this );
+
+        localChordId = chordOverlay.getLocalId();
+        // currentPredecessor = chordOverlay.getPredecessor();
+        // currentSuccessor = chordOverlay.getSuccessor();
 
         connectionMgr.addConnectionListener( this );
         dateFormat = new SimpleDateFormat( "yyyy.MM.dd HH:mm:ss.SSS Z" );
@@ -49,40 +66,73 @@ public class ConnectionDebugLogger implements ConnectionListener {
         }
     }
 
+    private void updateNeighborhoodIfChanged() {
+        if( null == currentPredecessor
+                || !currentPredecessor.equals( chordOverlay.getPredecessor() ) ) {
+            currentPredecessor = chordOverlay.getPredecessor();
+
+            if( null != currentPredecessor ) {
+                try {
+                    logWriter.append( dateFormat.format( new Date() ) + "|id:"
+                            + localChordId + "|chord:predecessor|predid:"
+                            + currentPredecessor + "\n" );
+                    logWriter.flush();
+                } catch ( IOException e ) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        if( null == currentSuccessor
+                || !currentSuccessor.equals( chordOverlay.getSuccessor() ) ) {
+            currentSuccessor = chordOverlay.getSuccessor();
+
+            if( null != currentSuccessor ) {
+                try {
+                    logWriter.append( dateFormat.format( new Date() ) + "|id:"
+                            + localChordId + "|chord:successor|predid:"
+                            + currentSuccessor + "\n" );
+                    logWriter.flush();
+                } catch ( IOException e ) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
     @Override
     public void connected( IConnectionManager connectionMgr,
             SocketAddress localAddress, SocketAddress remoteAddress ) {
 
-        try {
-            logWriter.append( dateFormat.format( new Date() )
-                    + " - outgoing connected - localaddr: " + localAddress
-                    + " remoteaddr: " + remoteAddress + "\n" );
-            logWriter.flush();
-        } catch ( IOException e ) {
-            e.printStackTrace();
-        }
+        updateNeighborhoodIfChanged();
     }
 
     @Override
     public void disconnected( IConnectionManager connectionMgr,
             SocketAddress localAddress, SocketAddress remoteAddress ) {
-        try {
-            logWriter.append( dateFormat.format( new Date() )
-                    + " - outgoing disconnected - localaddr: " + localAddress
-                    + " remoteaddr: " + remoteAddress + "\n" );
-            logWriter.flush();
-        } catch ( IOException e ) {
-            e.printStackTrace();
-        }
+        updateNeighborhoodIfChanged();
     }
 
     @Override
     public void clientConnected( IConnectionManager connectionMgr,
             SocketAddress localAddress, SocketAddress remoteAddress ) {
+        updateNeighborhoodIfChanged();
+    }
+
+    @Override
+    public void clientDisconnected( IConnectionManager connectionMgr,
+            SocketAddress localAddress, SocketAddress remoteAddress ) {
+        updateNeighborhoodIfChanged();
+    }
+
+    @Override
+    public void responsibilityChanged( IChordOverlay chord,
+            WrappedRange<IHash> from, WrappedRange<IHash> to ) {
         try {
             logWriter.append( dateFormat.format( new Date() )
-                    + " - incoming connected - localaddr: " + localAddress
-                    + " remoteaddr: " + remoteAddress + "\n" );
+ + "|id:"
+                    + localChordId + "|chord:respChanged|from:" + from + "|to:"
+                    + to + "\n" );
             logWriter.flush();
         } catch ( IOException e ) {
             e.printStackTrace();
@@ -90,12 +140,12 @@ public class ConnectionDebugLogger implements ConnectionListener {
     }
 
     @Override
-    public void clientDisconnected( IConnectionManager connectionMgr,
-            SocketAddress localAddress, SocketAddress remoteAddress ) {
+    public void receivedMessage( IChordOverlay chord, Object message,
+            IHash senderId ) {
         try {
-            logWriter.append( dateFormat.format( new Date() )
-                    + " - incoming disconnected - localaddr: " + localAddress
-                    + " remoteaddr: " + remoteAddress + "\n" );
+            logWriter.append( dateFormat.format( new Date() ) + "|id:"
+                    + localChordId + "|chord:messageRecv|sender:" + senderId
+                    + "\n" );
             logWriter.flush();
         } catch ( IOException e ) {
             e.printStackTrace();
