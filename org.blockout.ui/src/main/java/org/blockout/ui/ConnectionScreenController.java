044bfdbabaa295c301c591cee126c1ce209b7f36
coding-mof <coding.mof@gmail.com>
2012-07-25 19:51:01 +0200

    Implemented bootstrap within UI

--- org.blockout.ui/src/main/java/org/blockout/ui/ConnectionScreenController.java
+++ org.blockout.ui/src/main/java/org/blockout/ui/ConnectionScreenController.java
@@ -1,14 +1,20 @@
 package org.blockout.ui;
 
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.SocketAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.blockout.network.discovery.DiscoveryListener;
 import org.blockout.network.discovery.INodeDiscovery;
+import org.blockout.network.reworked.ConnectionManager;
 import org.blockout.world.IWorld;
 import org.blockout.world.LocalGameState;
 import org.newdawn.slick.state.GameState;
@@ -16,8 +22,11 @@ import org.newdawn.slick.state.StateBasedGame;
 import org.newdawn.slick.state.transition.FadeInTransition;
 import org.newdawn.slick.state.transition.FadeOutTransition;
 
+import com.google.common.base.Splitter;
+
 import de.lessvoid.nifty.Nifty;
 import de.lessvoid.nifty.controls.ListBox;
+import de.lessvoid.nifty.controls.TextField;
 import de.lessvoid.nifty.screen.Screen;
 
 /**
@@ -38,14 +47,17 @@ public class ConnectionScreenController implements StateBasedScreenController, D
 	protected INodeDiscovery		discovery;
 	protected IWorld				world;
 	protected LocalGameState		gameState;
+	private final ConnectionManager	connectionMgr;
 
 	protected List<SocketAddress>	knownNodes;
 
 	@Inject
-	public ConnectionScreenController(final LocalGameState gameState, final IWorld world, final INodeDiscovery discovery) {
+	public ConnectionScreenController(final LocalGameState gameState, final IWorld world,
+			final INodeDiscovery discovery, final ConnectionManager connectionMgr) {
 		this.gameState = gameState;
 		this.world = world;
 		this.discovery = discovery;
+		this.connectionMgr = connectionMgr;
 		knownNodes = new ArrayList<SocketAddress>( discovery.listNodes() );
 	}
 
@@ -70,6 +82,24 @@ public class ConnectionScreenController implements StateBasedScreenController, D
 
 	public void goBack() {
 		nifty.gotoScreen( "selectProfile" );
+	}
+
+	public void connectToIP() {
+		TextField field = screen.findNiftyControl( "txtIP", TextField.class );
+
+		Iterable<String> split = Splitter.on( ":" ).split( field.getText() );
+		Iterator<String> iter = split.iterator();
+		try {
+			String host = iter.next();
+			String port = iter.next();
+			connectionMgr.connectTo( new InetSocketAddress( InetAddress.getByName( host ), Integer.parseInt( port ) ) );
+		} catch ( NumberFormatException e ) {
+			e.printStackTrace();
+		} catch ( UnknownHostException e ) {
+			e.printStackTrace();
+		} catch ( NoSuchElementException e ) {
+			e.printStackTrace();
+		}
 	}
 
 	public void connectToPeer() {
